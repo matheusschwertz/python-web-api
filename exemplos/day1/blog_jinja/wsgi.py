86ff1070b237b94b85cf6fc5e93e0790eb2f7923
matheusschwertz <matheusschwertz@gmail.com>
2024-10-30 21:49:35 -0300

    adding my framework

--- exemplos/day1/blog_jinja/wsgi.py
+++ exemplos/day1/blog_jinja/wsgi.py
@@ -1,26 +1,37 @@
-import cgi
-import json
-from pathlib import Path
 from database import conn
-# NEW
-from jinja2 import Environment, FileSystemLoader
-# NEW
-env = Environment(loader=FileSystemLoader("templates"))
+from erik import Erik
 
-def add_new_post(post):
-    cursor = conn.cursor()
-    cursor.execute(
-        """\
-        INSERT INTO post (title, content, author)
-        VALUES (:title, :content, :author);
-        """,
-        post,
-    )
-    conn.commit()
+app = Erik()
 
-def render_template(template_name, **context):
-    template = env.get_template(template_name)
-    return template.render(**context).encode("utf-8")
+
+@app.route("^/$", template="list.template.html")
+def post_list():
+    posts = get_posts_from_database()
+    return {"post_list": posts}
+
+
+@app.route("^api/$")
+def post_list_api():
+    posts = get_posts_from_database()
+    return {"post_list": posts}, "200 OK", "application/json"
+
+
+@app.route(r"^/(?P<id>\d{1,})$", template="post.template.html")
+def post_detail(id):
+    post = get_posts_from_database(post_id=id)[0]
+    return {"post": post}
+
+
+@app.route("^/new$", template="form.template.html")
+def new_post_form():
+    return {}
+
+
+@app.route("^/new$", method="POST")
+def new_post_add(form):
+    post = {item.name: item.value for item in form.list}
+    add_new_post(post)
+    return "New post Created with Success!", "201 Created", "text/plain"
 
 
 def get_posts_from_database(post_id=None):
@@ -35,59 +46,16 @@ def get_posts_from_database(post_id=None):
     return [dict(zip(fields, post)) for post in results]
 
 
-def application(environ, start_response):
-    path = environ["PATH_INFO"]
-    method = environ["REQUEST_METHOD"]
-    body = b"Content Not Found"
-    status = "404 Not Found"
-    content_type = "text/html"
+def add_new_post(post):
+    cursor = conn.cursor()
+    cursor.execute(
+        """\
+        INSERT INTO post (title, content, author)
+        VALUES (:title, :content, :author);
+        """,
+        post,
+    )
+    conn.commit()
 
-    if path == "/" and method == "GET":
-        posts = get_posts_from_database()
-        body = render_template(
-            "list.template.html",
-            post_list=posts  # NEW
-        )
-        status = "200 OK"
-
-    elif path == "/api" and method == "GET":
-        posts = get_posts_from_database()
-        status = "200 OK"
-        body = json.dumps(posts).encode("utf-8")
-        content_type = "application/json"  # Serializacao
-
-    elif path.split("/")[-1].isdigit() and method == "GET":
-        post_id = path.split("/")[-1]
-        body = render_template(
-            "post.template.html",
-            post=get_posts_from_database(post_id=post_id)[0],
-        )
-        status = "200 OK"
-
-
-    elif path == "/new" and method == "POST":
-        form = cgi.FieldStorage(
-            fp=environ["wsgi.input"], environ=environ, keep_blank_values=1
-        )
-        post = {item.name: item.value for item in form.list}
-        add_new_post(post)
-        body = b"New post Created with Success!"
-        status = "201 Created"
-
-    elif path == "/new" and method == "GET":
-        body = render_template("form.template.html")
-        status = "200 OK"
-
-    headers = [("Content-type", content_type)]
-    start_response(status, headers)
-    return [body]
-
-############################################
-
-from erik import Erik
-
-app = Erik()
-
-@app.route("/")
-def index():
-    return "<strong>Hello</strong>"
+if __name__ == "__main__":
+    app.run()
